774cc4ae9c315a1fa68fc8b03f48d68b62f1f3e9
davier enrique perez cabrera <davierperez11.2@gmail.com>
2024-12-10 22:24:37 -0500

    feat: implementing singleton pattern to connection to data base

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,6 +1,53 @@
-import { Sequelize } from 'sequelize';
+import {Sequelize} from 'sequelize';
 import {DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER_NAME} from "../config";
+import {ConnectionRepository} from "../interfaces/ConnectionRepository";
 
+class ConnectionMysql implements ConnectionRepository {
+    private static connectionMysql: ConnectionMysql;
+    private static configDB: Sequelize;
+    constructor() {
+        if(!ConnectionMysql.connectionMysql) {
+            this.configDataBase();
+            ConnectionMysql.connectionMysql = this;
+        }
+        return ConnectionMysql.connectionMysql;
+    }
+
+    protected configDataBase() {
+        ConnectionMysql.configDB = new Sequelize(
+            DB_NAME,
+            DB_USER_NAME,
+            DB_PASSWORD,
+            {
+                dialect: 'mysql',
+                host: DB_HOST,
+                port: DB_PORT
+            });
+    }
+
+    connect(): void {
+        ConnectionMysql.configDB.authenticate().then(() => {
+            console.log('Connection has been established successfully.');
+        }).catch((error) => {
+            console.error('Unable to connection to the database: ', error);
+            this.disconnect();
+        });
+    }
+
+    connection(): Sequelize {
+        return ConnectionMysql.configDB;
+    }
+
+    disconnect(): void {
+        ConnectionMysql.configDB.close().then(() => {
+            console.log("connections to the data base closed");
+        }).catch();
+    }
+
+}
+
+export default new ConnectionMysql();
+/*
 export const configDB = new Sequelize(
     DB_NAME,
     DB_USER_NAME,
@@ -18,3 +65,9 @@ export function connect() {
         console.error('Unable to connection to the database: ', error);
     });
 }
+
+export function desconnect(): void {
+    configDB.close().then(() => {
+        console.log("connections to the data base closed");
+    }).catch();
+}*/
