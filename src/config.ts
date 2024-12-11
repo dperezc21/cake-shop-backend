53215ae92fe53041a949bb09f0d5a80bc39af973
davier enrique perez cabrera <davierperez11.2@gmail.com>
2024-12-11 17:26:04 -0500

    feat: using .env file

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,7 @@
 
 export const PORT: number = 3000;
-
-export const DB_NAME: string = process.env.DB_NAME || 'cake_shop';
-export const DB_USER_NAME: string = process.env.DB_USER_NAME || 'root';
-export const DB_PASSWORD: string = process.env.DB_PASSWORD || '';
-export const DB_HOST: string = process.env.DB_HOST || 'localhost';
-export const DB_PORT: number = Number(process.env.DB_PORT) || 3306;
+export const DB_NAME: string = process.env.DB_NAME;
+export const DB_USER_NAME: string = process.env.DB_USER_NAME;
+export const DB_PASSWORD: string = process.env.DB_PASSWORD;
+export const DB_HOST: string = process.env.DB_HOST;
+export const DB_PORT: number = Number(process.env.DB_PORT);
