b6f580141b7acfeb725f8024f6520128e001968c
davier enrique perez cabrera <davierperez11.2@gmail.com>
2024-12-19 16:14:18 -0500

    feat: add column in user that do reference to user rol table

--- src/db/create-tables.db.ts
+++ src/db/create-tables.db.ts
@@ -3,6 +3,7 @@ import {createUserTable} from "../models/user.model";
 import {createCakeImageTable} from "../models/cake-image.model";
 import {createCakeTable} from "../models/cake.model";
 import {CreateTablesError} from "../utils/exceptions/create-tables-error";
+import {addColumn, createUserRolTable} from "../models/user-rol.model";
 
 export class CreateTablesDb {
 
@@ -12,6 +13,8 @@ export class CreateTablesDb {
             await createUserTable();
             await createCakeTable();
             await createCakeImageTable();
+            await createUserRolTable();
+            addColumn();
         } catch (err) {
             throw new CreateTablesError("Error while create tables in data base");
         }
---
+++ src/models/user-rol.model.ts
@@ -0,0 +1,35 @@
+import connection from "../db/connection";
+import connectionDataBase from "../db/connection";
+import {DataTypes} from "sequelize";
+
+const queryInterface = connectionDataBase.connection().getQueryInterface();
+
+export const UserRolModel = connection.connection().define('UserRol',{
+    rol_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+        unique: true
+    }
+}, {
+    tableName: 'user_rol'
+});
+
+export function addColumn() {
+    queryInterface.addColumn({ tableName: 'user' },
+        'user_rol_name', {
+        type: DataTypes.STRING,
+        references: {
+            model: { tableName: 'user_rol' },
+            key: 'rol_name'
+        }
+    }).then(value => console.log("column added to use"))
+}
+
+export async function createUserRolTable() {
+    return new Promise(async(resolve, reject) => {
+        UserRolModel.sync({alter: true})
+            .then(value => resolve('user rol table created successfully!'))
+            .catch(reason => reject('Unable to create user rol table'));
+    });
+}
